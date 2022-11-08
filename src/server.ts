09f347cd477b51e381993841172b7e6381bb1671
ilemuelfigueira <lfigueiradev@gmail.com>
2022-11-08 17:43:10 -0300

    ajustes server.ts
    adicionando rotas users, games, guesses

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,18 @@ import { poolsRoutes } from '@controllers/pools';
 import { errorHandler } from '@errors/handler';
 
 import express from 'express';
+import { gamesRoutes } from './controllers/games';
+import { guessesRoutes } from './controllers/guesses';
+import { usersRoutes } from './controllers/users';
 
 const app = express();
 
 app.use(express.json());
-app.use(errorHandler)
+app.use(errorHandler);
 app.use(poolsRoutes);
+app.use(usersRoutes);
+app.use(gamesRoutes);
+app.use(guessesRoutes);
 
 export {
   app
